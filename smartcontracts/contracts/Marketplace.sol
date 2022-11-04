7d438d2934800cef05144390a9b1ac44c5945635
franciscom-bootcamp <encode@mail.fmf.rocks>
2022-11-04 17:49:13 +0000

    Hardhat project initial import

---
+++ smartcontracts/contracts/Marketplace.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "hardhat/console.sol";
+
+contract Marketplace is ReentrancyGuard {
+    
+}
