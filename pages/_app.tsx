4235127be0f64ce7b719b98bd8776fa52bb40b30
Patrick Asare <8025627+patrickAsare@users.noreply.github.com>
2022-11-06 17:21:59 +0000

    Wallet connect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,34 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Web3Modal } from '@web3modal/react';
+import { chains, providers } from '@web3modal/ethereum';
+
+// Get projectID at https://cloud.walletconnect.com
+if (!process.env.NEXT_PUBLIC_WALLET_CONNECT_ID)
+  throw new Error('You need to provide NEXT_PUBLIC_PROJECT_ID env variable');
+
+// Configure web3modal
+const modalConfig = {
+  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_ID,
+  theme: 'dark' as const,
+  accentColor: 'default' as const,
+  ethereum: {
+    appName: 'web3Modal',
+    autoConnect: true,
+    chains: [chains.goerli],
+    providers: [
+      providers.walletConnectProvider({
+        projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_ID,
+      }),
+    ],
+  },
+};
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Web3Modal config={modalConfig} />
+    </>
+  );
 }
